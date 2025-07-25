f81a929ae4b02c7b57806be7346c6f3a6984786f
lamtrinhh1201 <lamtrinhh1201@gmail.com>
2025-07-25 13:41:07 +0700

    add migration and update db

---
+++ Group1_PRN222/Migrations/20250725063619_blablabla.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Group1_PRN222.Migrations
+{
+    /// <inheritdoc />
+    public partial class blablabla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isDefault",
+                table: "Address",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isDefault",
+                table: "Address",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
